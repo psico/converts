52b2f618e53397a474cb4659789d99ad1281542d
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-08-27 21:54:30 -0300

    feature: adiciona script para obter data atual

---
+++ src/js/home.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const currentDateElement = document.getElementById("current-date-element");
+  const currentDate = new Date().toLocaleString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+
+  currentDateElement.textContent = `Data: ${currentDate}`;
+  currentDate.datetime = currentDate;
+});
