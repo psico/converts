6e4f98cf49f2461d911dc80bee31ba86a8f15cff
gabrielTavaresFP <gabrieltavares8182pessoal@gmail.com>
2025-09-16 14:15:29 -0300

    colocando data atual

--- src/js/home.js
+++ src/js/home.js
@@ -118,3 +118,14 @@ document.addEventListener("DOMContentLoaded", () => {
 function TrocaValores() {
   console.log("Oi");
 };
+
+  function dataAtual() {
+    const hoje = new Date();
+    const dia = String(hoje.getDate()).padStart(2, '0');
+    const mes = String(hoje.getMonth() + 1).padStart(2, '0');
+    const ano = hoje.getFullYear();
+
+    document.getElementById("data-atual").innerText = `${dia}/${mes}/${ano}`;
+  }
+
+  document.getElementById("data-atual").addEventListener("click", dataAtual);
