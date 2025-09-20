021ff58a0443f1998586267add1180de8993efdb
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-09-20 00:08:52 -0300

    feat: adiciona gráfico de cotação do dólar

--- src/js/home/graph-section.js
+++ src/js/home/graph-section.js
@@ -1,7 +1,46 @@
 import CurrencyService from "../services/currency-service.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const currencyService = new CurrencyService();
 
-  currencyService.getMonthlyDollarRate();
+  const monthlyDollarRateData = await currencyService.getMonthlyDollarRate();
+
+  const dollarRatesDates = Object.keys(monthlyDollarRateData.rates);
+
+  const formattedDollarRatesDates = dollarRatesDates.map((dollarRateDate) => {
+    const formattedDollarRateDate = new Date(dollarRateDate).toLocaleString(
+      "pt-BR",
+      {
+        month: "2-digit",
+        day: "2-digit",
+      }
+    );
+
+    return formattedDollarRateDate;
+  });
+
+  const dollarRatesValues = Object.values(monthlyDollarRateData.rates);
+
+  const formattedDollarRatesValues = dollarRatesValues.map((dollaRateValue) => {
+    const formattedDollarRateValue = Number(dollaRateValue.BRL).toFixed(2);
+
+    return formattedDollarRateValue;
+  });
+
+  const canvasGraph = document.getElementById("canvas-graph");
+
+  (async function () {
+    new Chart(canvasGraph, {
+      type: "bar",
+      data: {
+        labels: formattedDollarRatesDates,
+        datasets: [
+          {
+            label: "Cotação do Dólar nos últimos 30 dias",
+            data: formattedDollarRatesValues,
+          },
+        ],
+      },
+    });
+  })();
 });
